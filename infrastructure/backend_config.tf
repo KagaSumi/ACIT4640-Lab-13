24c24bd39b99f317f277bda060051b18421f6b3c
Quinten Leung <Quinten.Leung@gmail.com>
2024-12-05 16:14:42 -0800

    Remove bucket

--- infrastructure/backend_config.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket         = "skibidimaxxingbucket"
-        key            = "terraform.tfstate"
-        dynamodb_table = "mod-remote-state-lock"
-        region         =  "us-west-2" 
-        encrypt        = true
-    }
-}
